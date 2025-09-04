576c052b89783aeb63891ff306c7aa2cf35563b7
mahmoud ezz <leadbylead1@gmail.com>
2025-09-04 16:29:50 +0000

    3rd face content edit
    let's make 3rd face only 4 words max say a vlue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ export default {
         },
         'float': {
           '0%, 100%': {
-            transform: 'translateY(0)',
+            transform: 'translateY(0px)',
           },
           '50%': {
             transform: 'translateY(-10px)',
