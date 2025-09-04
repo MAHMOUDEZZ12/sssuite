54473cd1e153c765db6222b75ded4e325e4ae74f
mahmoud ezz <leadbylead1@gmail.com>
2025-09-04 21:26:50 +0000

    now invstgate all the rquirements and api working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,12 +11,12 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCiA6-YXtXlD4sy0soqPG6QdTcZTz36Aoc",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  authDomain: "leadbylead-b79ea.firebaseapp.com",
+  projectId: "leadbylead-b79ea",
+  storageBucket: "leadbylead-b79ea.appspot.com",
+  messagingSenderId: "1035949656663",
+  appId: "1:1035949656663:web:9614a7e7297e28b105d15a",
+  measurementId: "G-K72V5223GN"
 };
 
 // Initialize Firebase
