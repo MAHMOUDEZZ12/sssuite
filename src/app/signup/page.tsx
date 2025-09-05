bdae9ba76a1668a6d68bae66450e2d7eaae39f86
mahmoud ezz <leadbylead1@gmail.com>
2025-09-05 03:50:57 +0000

    user who come from the pricing direcly better to colllect payment method

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,16 +20,6 @@ export default function SignupPage() {
             </p>
           </div>
           <div className="grid gap-4">
-            <div className="grid grid-cols-2 gap-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="first-name">First name</Label>
-                  <Input id="first-name" placeholder="Max" required />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="last-name">Last name</Label>
-                  <Input id="last-name" placeholder="Robinson" required />
-                </div>
-            </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
